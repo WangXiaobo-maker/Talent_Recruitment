1878abaea8bc4558196e08e9f0bfba36756c5d8a
WangXiaobo-maker <1299784636@qq.com>
2020-08-19 15:12:15 +0800

    testUpload

---
+++ src/main/java/com/talresource/Talent_Recruitment/dao/TestDao.java
@@ -0,0 +1,13 @@
+package com.talresource.Talent_Recruitment.dao;
+
+import com.talresource.Talent_Recruitment.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface TestDao {
+
+    @Select("select * from user")
+    User selectAllUser();
+
+}
