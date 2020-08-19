a71e7529fdccd8f180af0a03b65f1d72f22710c1
WangXiaobo-maker <1299784636@qq.com>
2020-08-19 14:21:08 +0800

    Commit Message

---
+++ src/main/java/com/talresource/Talent_Recruitment/TalentRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.talresource.Talent_Recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentRecruitmentApplication.class, args);
+	}
+
+}
